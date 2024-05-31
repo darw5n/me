362e2e9dca72efa116867ef4566ebb96e703390c
Darwin <darwin.vegher@gmail.com>
2024-05-31 13:06:15 +0200

    revert old site

--- Script/email.js
+++
@@ -1,9 +0,0 @@
-var user = "darwin.vegher"; // Nome utente
-var domain = "gmail.com"; // Dominio
-var element = document.getElementById("email"); // ID dell'elemento dove vuoi visualizzare l'email
-
-// Costruisci l'indirizzo email
-var email = user + "@" + domain;
-
-// Sostituisci il contenuto dell'elemento con l'indirizzo email
-element.innerHTML = '<a href="mailto:' + email + '">' + email + "</a>";
